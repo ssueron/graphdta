12463e7bd2b7f81d4c793bf4b3c88fa274951b78
ssueron <sebastien.sueron@orange.fr>
2025-10-13 19:03:11 +0200

    ].f[.a[wed

--- models/protein_cnn_blosum.py
+++ models/protein_cnn_blosum.py
@@ -45,84 +45,33 @@ def create_blosum62_embedding(num_tokens=22, embed_dim=128):
     return embedding, projection
 
 class DeepProteinCNN_BLOSUM(nn.Module):
-    def __init__(self, num_features_xt=21, embed_dim=128, output_dim=128, dropout=0.1):
+    def __init__(self, num_features_xt=21, embed_dim=128, n_filters=32, kernel_size=8,
+                 output_dim=128, dropout=0.2, seq_len=85):
         super(DeepProteinCNN_BLOSUM, self).__init__()
 
         self.embedding, self.projection = create_blosum62_embedding(num_features_xt + 1, embed_dim)
-
-        self.conv1_1 = nn.Conv1d(embed_dim, 32, kernel_size=4)
-        self.conv1_2 = nn.Conv1d(32, 32, kernel_size=4)
-        self.conv1_3 = nn.Conv1d(32, 32, kernel_size=4)
-        self.pool1 = nn.MaxPool1d(2)
-
-        self.conv2_1 = nn.Conv1d(32, 64, kernel_size=4)
-        self.conv2_2 = nn.Conv1d(64, 64, kernel_size=4)
-        self.conv2_3 = nn.Conv1d(64, 64, kernel_size=4)
-        self.pool2 = nn.MaxPool1d(2)
-
-        self.conv3_1 = nn.Conv1d(64, 96, kernel_size=4)
-        self.conv3_2 = nn.Conv1d(96, 96, kernel_size=4)
-        self.conv3_3 = nn.Conv1d(96, 96, kernel_size=4)
-        self.pool3 = nn.MaxPool1d(2)
-
+        self.conv = nn.Conv1d(in_channels=embed_dim, out_channels=n_filters, kernel_size=kernel_size)
         self.dropout = nn.Dropout(dropout)
+        self.seq_len = seq_len
 
         self._calc_fc_input_dim()
         self.fc = nn.Linear(self.fc_input_dim, output_dim)
 
     def _calc_fc_input_dim(self):
-        x = torch.zeros(1, 85)
-        x = self.embedding(x.long())
+        x = torch.zeros(1, self.seq_len).long()
+        x = self.embedding(x)
         x = self.projection(x)
         x = x.permute(0, 2, 1)
-
-        x = F.relu(self.conv1_1(x))
-        x = F.relu(self.conv1_2(x))
-        x = F.relu(self.conv1_3(x))
-        x = self.pool1(x)
-
-        x = F.relu(self.conv2_1(x))
-        x = F.relu(self.conv2_2(x))
-        x = F.relu(self.conv2_3(x))
-        x = self.pool2(x)
-
-        x = F.relu(self.conv3_1(x))
-        x = F.relu(self.conv3_2(x))
-        x = F.relu(self.conv3_3(x))
-        x = self.pool3(x)
-
-        self.fc_input_dim = x.view(1, -1).size(1)
+        x = F.relu(self.conv(x))
+        x = x.view(x.size(0), -1)
+        self.fc_input_dim = x.size(1)
 
     def forward(self, x):
         x = self.embedding(x)
         x = self.projection(x)
         x = x.permute(0, 2, 1)
-
-        x = F.relu(self.conv1_1(x))
+        x = F.relu(self.conv(x))
         x = self.dropout(x)
-        x = F.relu(self.conv1_2(x))
-        x = self.dropout(x)
-        x = F.relu(self.conv1_3(x))
-        x = self.pool1(x)
-        x = self.dropout(x)
-
-        x = F.relu(self.conv2_1(x))
-        x = self.dropout(x)
-        x = F.relu(self.conv2_2(x))
-        x = self.dropout(x)
-        x = F.relu(self.conv2_3(x))
-        x = self.pool2(x)
-        x = self.dropout(x)
-
-        x = F.relu(self.conv3_1(x))
-        x = self.dropout(x)
-        x = F.relu(self.conv3_2(x))
-        x = self.dropout(x)
-        x = F.relu(self.conv3_3(x))
-        x = self.pool3(x)
-        x = self.dropout(x)
-
         x = x.view(x.size(0), -1)
         x = self.fc(x)
-
         return x
